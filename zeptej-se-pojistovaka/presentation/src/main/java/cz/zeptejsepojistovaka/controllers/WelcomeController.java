4eb76dcbd6611649260fe5de5e912e48bbc2a65b
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-09-12 11:03:40 +0200

    refactored a publicHomepage processing

--- zeptej-se-pojistovaka/presentation/src/main/java/cz/zeptejsepojistovaka/controllers/WelcomeController.java
+++ zeptej-se-pojistovaka/presentation/src/main/java/cz/zeptejsepojistovaka/controllers/WelcomeController.java
@@ -12,12 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
  */
 @Controller
-public class PublicHomepageController {
+public class WelcomeController {
 
-    private final Logger log = Logger
-            .getLogger("cz.zeptejsepojistovaka.controllers.PublicHomepageController");
+    private final Logger log = Logger.getLogger(WelcomeController.class);
 
-    @RequestMapping(value = "/public-homepage/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String init(HttpServletRequest request, ModelMap model) {
         this.log.debug("in Controller");
         return "publicHomepage";
--- zeptej-se-pojistovaka/presentation/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page pageEncoding="UTF-8"%>
-<%@ include file="/WEB-INF/jsp/include-preceding-html.jsp"%>
-
-<%-- Forwards processing since we can't set the welcome page in web.xml to a "virtual" URL. --%>
-<jsp:forward page="/public-homepage/" />
