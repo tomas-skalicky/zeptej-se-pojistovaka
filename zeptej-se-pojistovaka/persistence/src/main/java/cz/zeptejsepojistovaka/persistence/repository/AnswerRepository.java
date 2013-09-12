7cb05f0916a412a5652c34cf83ec9282e99f2e7b
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-09-12 14:26:49 +0200

    fixed a bug in AnswerRepository

--- zeptej-se-pojistovaka/persistence/src/main/java/cz/zeptejsepojistovaka/persistence/repository/AnswerRepository.java
+++ zeptej-se-pojistovaka/persistence/src/main/java/cz/zeptejsepojistovaka/persistence/repository/AnswerRepository.java
@@ -2,7 +2,7 @@ package cz.zeptejsepojistovaka.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import cz.zeptejsepojistovaka.domainmodel.ContributionThread;
+import cz.zeptejsepojistovaka.domainmodel.Answer;
 
-public interface AnswerRepository extends JpaRepository<ContributionThread, Integer> {
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 }
