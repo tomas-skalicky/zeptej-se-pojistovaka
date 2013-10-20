6a203172914015d46060798293753fa0a8fe5f65
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-10-20 14:26:18 +0200

    refactored DataSourceProperties

--- zeptej-se-pojistovaka/persistence/src/main/java/cz/zeptejsepojistovaka/persistence/config/DataSourceProperties.java
+++ zeptej-se-pojistovaka/persistence/src/main/java/cz/zeptejsepojistovaka/persistence/config/DataSourceProperties.java
@@ -1,16 +1,9 @@
 package cz.zeptejsepojistovaka.persistence.config;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component(value = DataSourceProperties.COMPONENT_NAME)
@@ -27,51 +20,37 @@ public class DataSourceProperties {
 
     public static final String COMPONENT_NAME = "dataSourceProperties";
 
-    private static final String PROPERTY_FILE_CLASSPATH = "environment/dataSource.properties";
+    @NonNull
+    @Getter
+    @Value("${" + DATABASE_DRIVER_CLASS_PROPERTY_NAME + "}")
+    private String databaseDriverClass;
 
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
-    private String databaseDriverClass;
-    @NonNull
-    @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("${" + DATABASE_CONNECTION_URL_PROPERTY_NAME + "}")
     private String databaseConnectionUrl;
+
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("${" + DATABASE_USERNAME_PROPERTY_NAME + "}")
     private String databaseUsername;
+
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("${" + DATABASE_PASSWORD_PROPERTY_NAME + "}")
     private String databasePassword;
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("${" + HIBERNATE_DIALECT_PROPERTY_NAME + "}")
     private String hibernateDialect;
+
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("#{new Boolean('${" + HIBERNATE_SHOW_SQL_PROPERTY_NAME + "}')}")
     private boolean hibernateShowSql;
+
     @NonNull
     @Getter
-    @Setter(AccessLevel.PRIVATE)
+    @Value("${" + HIBERNATE_HBM2DDL_AUTO_PROPERTY_NAME + "}")
     private String hibernateHbm2ddlAuto;
-
-    public DataSourceProperties() throws IOException {
-        loadPropertyValuesFromFile(PROPERTY_FILE_CLASSPATH);
-    }
-
-    private void loadPropertyValuesFromFile(String propertyFileClassPath) throws IOException {
-        Resource propertyFile = new ClassPathResource(propertyFileClassPath);
-        Properties loadedProperties = PropertiesLoaderUtils.loadProperties(propertyFile);
-
-        setDatabaseDriverClass(loadedProperties.getProperty(DATABASE_DRIVER_CLASS_PROPERTY_NAME));
-        setDatabaseConnectionUrl(loadedProperties.getProperty(DATABASE_CONNECTION_URL_PROPERTY_NAME));
-        setDatabaseUsername(loadedProperties.getProperty(DATABASE_USERNAME_PROPERTY_NAME));
-        setDatabasePassword(loadedProperties.getProperty(DATABASE_PASSWORD_PROPERTY_NAME));
-        setHibernateDialect(loadedProperties.getProperty(HIBERNATE_DIALECT_PROPERTY_NAME));
-        setHibernateShowSql(Boolean.valueOf(loadedProperties.getProperty(HIBERNATE_SHOW_SQL_PROPERTY_NAME)));
-        setHibernateHbm2ddlAuto(loadedProperties.getProperty(HIBERNATE_HBM2DDL_AUTO_PROPERTY_NAME));
-    }
 }
